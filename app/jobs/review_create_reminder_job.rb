2b1ec68a7a709c0656ce6f98b43a6b1cf0b01747
Gloria Ding <gloria.hc.ding@gmail.com>
2018-01-14 15:02:09 -0800

    experiment time to delay sending emails

--- app/jobs/review_create_reminder_job.rb
+++ app/jobs/review_create_reminder_job.rb
@@ -4,5 +4,7 @@ class ReviewCreateReminderJob < ApplicationJob
   def perform(*args)
     # Do something later
     ReviewMailer.notify_product_owner(Review.last).deliver_now
+    # ReviewMailer.notify_product_owner(Review.last).deliver_later(wait: 10.seconds)
+    # ReviewMailer.notify_product_owner(Review.last).deliver_later(wait: 2.minutes)
   end
 end
